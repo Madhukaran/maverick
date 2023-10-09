16d5a088befbcf3e889399596091e23f9f4d1954
madhukaran <madhukaran449@gmail.com>
2023-10-09 19:09:28 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: '/maverick',
+    assetPrefix: '/maverick/',
+    output: 'export',
+}
 
 module.exports = nextConfig
