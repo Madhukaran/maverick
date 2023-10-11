5ca74b16557c5df1c7a99fc3086bf536bceae828
madhukaran <madhukaran449@gmail.com>
2023-10-12 00:52:39 +0530

    updated login page

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="flex flex-col w-screen p-2 gap-1">
         <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New Match</button>
         <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Open Match</button>
-        <p className="text-sm text-center italic m-2">Wa'nna try? <a>Signup</a> / <a>Login</a></p>
+        <p className="text-sm text-center italic m-2">Wa'nna try? <a href="justdial.com">Signup</a> / <a>Login</a></p>
       </div>
     </main>
   )
