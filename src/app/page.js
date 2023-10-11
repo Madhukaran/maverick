3ca2bae315e4f6b1209f333fc942af15e4cf7b6f
madhukaran <madhukaran449@gmail.com>
2023-10-11 20:44:36 +0530

    updated login page

--- src/app/page.js
+++ src/app/page.js
@@ -11,10 +11,15 @@ export default function Home() {
   const { View } = useLottie(options);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {View}
-      <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New Match</button>
-      <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Open Match</button>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="flex grow">
+        {View}
+      </div>
+      <div className="flex flex-col w-screen p-2 gap-1">
+        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New Match</button>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Open Match</button>
+        <p>Wa'nna try? Signup / Login</p>
+      </div>
     </main>
   )
 }
